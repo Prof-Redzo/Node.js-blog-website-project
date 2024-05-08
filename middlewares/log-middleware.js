946ac009c3ada505140a39da204f33fa3c4b0451
Redžo <154461862+Prof-Redzo@users.noreply.github.com>
2024-05-08 09:54:01 +0200

    added routes and middleware

--- middlewares/log-middleware.js
+++ middlewares/log-middleware.js
@@ -1,4 +1,20 @@
+import jwt from "jsonwebtoken";
+
 export const logger = (req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 };
+
+if(!token) {
+  return res.status(401).send("Unauthorized");
+ }
+
+ try{
+   const decoded = jwt.verify(token, process.env.JWT_KEY);
+   req.user = decoded;
+   next();
+ }catch(e){
+   return res.status(401).send("Invalid token");
+ };
+
+export default verify;
